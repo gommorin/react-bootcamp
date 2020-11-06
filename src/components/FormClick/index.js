3073662422e0029edaa66ba8fb36be443858feb9
gommorin <luis.gomezmorin@gmail.com>
2020-11-05 19:19:07 -0600

    clase 8

--- src/components/FormClick/index.js
+++ src/components/FormClick/index.js
@@ -4,33 +4,8 @@ import React, { Component } from 'react'
 import './FormClick.css'
 
 // FORMULARIO PARA CAMBIO DE DIVISAS ENTRE PESOS Y DOLARES //
-class Form extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isActive: false,
-      MXN: '',
-      USD: ''
-    }
-
-    this.handleChange = this.handleChange.bind(this) // ---------> QUÉ ES ESTO???
-  }
-
-  handleChange() {
-    this.setState({
-      USD: value / 21.6
-    })
-  }
-
-  render() {
-    const {MXN, USD} = this.state
-    return <div>
-      <form>
-        <input type='number' placeholder="MXN $"/>        <button>Cambiar a USD</button>
-      </form>
-      <p>USD: ${USD}</p>
-    </div>
-  }
+class FormClick extends Component {
+  
 }
 
-export default Form
+export default FormClick
